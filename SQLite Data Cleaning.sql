ac75abb0ffebcc425dc85f987e9f1e5bd2a01f31
Zach Wong <wzhenngie@gmail.com>
2024-07-31 20:19:59 +0800

    Minor fixes to query

--- SQLite Data Cleaning.sql
+++ SQLite Data Cleaning.sql
@@ -1,8 +1,9 @@
-/* Data Cleaning in SQL Queries */
+/* Data Cleaning in SQL Queries 
+ * Conducted in SQLite */
 
 SELECT 
   *
-FROM Nashville_Housing_Data_for_Data_Cleaning nhdfdc 
+FROM Nashville_Housing_Data
 LIMIT
   1000
   
@@ -13,16 +14,16 @@ LIMIT
 SELECT  
   SaleDate
 FROM
-  Nashville_Housing_Data_for_Data_Cleaning 
+  Nashville_Housing_Data
 
 -- If it does not update properly:
 ALTER TABLE
-  Nashville_Housing_Data_for_Data_Cleaning
+  Nashville_Housing_Data
 ADD 
   SalesDate_Converted Date
   
 UPDATE 
-  Nashville_Housing_Data_for_Data_Cleaning 
+  Nashville_Housing_Data
 SET  
   SalesDate_Converted = CONVERT(Date, SaleDate)
 
@@ -32,10 +33,10 @@ SELECT
   SaleDate, 
   CONVERT(datetime, SaleDate, 103) AS Date
 FROM
-  Nashville_Housing_Data_for_Data_Cleaning 
+  Nashville_Housing_Data
   
 UPDATE
-  Nashville_Housing_Data_for_Data_Cleaning 
+  Nashville_Housing_Data
 SET 
   SaleDate = CONVERT(Date,SaleDate)
 
@@ -65,20 +66,20 @@ SELECT
      END
   ) AS standardized_date
 FROM 
-  Nashville_Housing_Data_for_Data_Cleaning
+  Nashville_Housing_Data
   
 /* instr(column, "string"/value) > 0 THEN 'Y' checks if 'Month' is in the SaleDate string column, If found, it returns a position greater than 0, 
  indicating the month is "January", which is then converted to '01'. */
   
 -- Update the new date format into the Table (Step 1: insert new column)
 ALTER TABLE
-  Nashville_Housing_Data_for_Data_Cleaning
+  Nashville_Housing_Data
 ADD COLUMN
   Standardized_Date TEXT; -- if other SQL Servers like MySQL/PostgreSQL apply DATE instead of TEXT
 
 -- Update (Step 2: Add permanently into the new column with UPDATE SET Clause)
 UPDATE 
-  Nashville_Housing_Data_for_Data_Cleaning 
+  Nashville_Housing_Data
 SET
   Standardized_Date = (substr(SaleDate, -4, 4) || '-' || 
    CASE 
@@ -110,7 +111,7 @@ SET
 SELECT 
   PropertyAddress 
 FROM 
-  Nashville_Housing_Data_for_Data_Cleaning
+  Nashville_Housing_Data
 WHERE  
   PropertyAddress == "" -- [PropertyAdress IS NULL] for other SQL Servers
  
@@ -119,7 +120,7 @@ SELECT
   PropertyAddress,
   ParcelID 
 FROM
-  Nashville_Housing_Data_for_Data_Cleaning
+  Nashville_Housing_Data
 ORDER BY
   ParcelID 
   
@@ -130,8 +131,8 @@ SELECT
   b.ParcelID,
   b.PropertyAddress,
   IFNULL(a.PropertyAddress, b.PropertyAddress) -- other SQL Server, use ISNULL
-FROM Nashville_Housing_Data_for_Data_Cleaning AS a
-JOIN Nashville_Housing_Data_for_Data_Cleaning AS b
+FROM Nashville_Housing_Data AS a
+JOIN Nashville_Housing_Data AS b
   ON a.ParcelID = b.ParcelID 
   AND a."UniqueID " <> b."UniqueID " -- parsing same table itself, while still separating rows of repeating ParcelIDs with UniqueID
 WHERE
@@ -148,8 +149,8 @@ SELECT
   	THEN b.PropertyAddress
   	ELSE a.PropertyAddress
   END AS Address
-FROM Nashville_Housing_Data_for_Data_Cleaning AS a
-JOIN Nashville_Housing_Data_for_Data_Cleaning AS b
+FROM Nashville_Housing_Data AS a
+JOIN Nashville_Housing_Data AS b
   ON a.ParcelID = b.ParcelID 
   AND a."UniqueID " <> b."UniqueID " -- parsing same table itself, while still separating repeating ParcelIDs with UniqueID
 WHERE
@@ -160,25 +161,25 @@ UPDATE a
 SET 
   PropertyAddress = ISNULL(a.PropertyAddress,b.PropertyAddress)
 FROM
-  PortfolioProject.dbo.NashvilleHousing a
+  Nashville_Housing_Data AS a
 JOIN 
-  PortfolioProject.dbo.NashvilleHousing b
+  Nashville_Housing_Data AS b
 	ON a.ParcelID = b.ParcelID
-	AND a.[UniqueID ] <> b.[UniqueID ]
+	AND a."UniqueID " <> b."UniqueID "
 WHERE 
   a.PropertyAddress is null
   
 -- SQLite version to update through subquery
-UPDATE Nashville_Housing_Data_for_Data_Cleaning
+UPDATE Nashville_Housing_Data
 SET PropertyAddress = (
   SELECT 
     b.PropertyAddress
   FROM 
-    Nashville_Housing_Data_for_Data_Cleaning AS b
+    Nashville_Housing_Data AS b
   WHERE 
-    Nashville_Housing_Data_for_Data_Cleaning.ParcelID = b.ParcelID
+    Nashville_Housing_Data.ParcelID = b.ParcelID
   AND 
-    Nashville_Housing_Data_for_Data_Cleaning."UniqueID " <> b."UniqueID "
+    Nashville_Housing_Data."UniqueID " <> b."UniqueID "
   LIMIT 1
 )
 WHERE PropertyAddress = ""
@@ -190,31 +191,31 @@ WHERE PropertyAddress = ""
 SELECT 
   PropertyAddress 
 FROM 
-  Nashville_Housing_Data_for_Data_Cleaning 
+  Nashville_Housing_Data
   
 SELECT 
   SUBSTR(PropertyAddress,1,INSTR(PropertyAddress,',')- 1) AS Address, --SQLite ver of SUBSTRING and CHARINDEX
   SUBSTR(PropertyAddress, INSTR(PropertyAddress, ',')+ 1, LENGTH(PropertyAddress)) AS City --other SQL uses LEN() instead of LENGTH
 FROM
-  Nashville_Housing_Data_for_Data_Cleaning  
+  Nashville_Housing_Data
 
 ALTER TABLE
-  Nashville_Housing_Data_for_Data_Cleaning
+  Nashville_Housing_Data
 ADD COLUMN
   StreetAddress TEXT; 
   
 ALTER TABLE
-  Nashville_Housing_Data_for_Data_Cleaning
+  Nashville_Housing_Data
 ADD COLUMN
   City TEXT; 
   
 UPDATE 
-  Nashville_Housing_Data_for_Data_Cleaning 
+  Nashville_Housing_Data 
 SET 
   StreetAddress = SUBSTR(PropertyAddress,1,INSTR(PropertyAddress,',')- 1)
   
 UPDATE 
-  Nashville_Housing_Data_for_Data_Cleaning 
+  Nashville_Housing_Data 
 SET   
   City = SUBSTR(PropertyAddress, INSTR(PropertyAddress, ',')+ 1, LENGTH(PropertyAddress))
   
@@ -227,49 +228,49 @@ SET
 SELECT 
   OwnerAddress 
 FROM 
-  Nashville_Housing_Data_for_Data_Cleaning  
+  Nashville_Housing_Data 
 
 SELECT
   PARSENAME(REPLACE(OwnerAddress, ',', '.') , 3),
   PARSENAME(REPLACE(OwnerAddress, ',', '.') , 2),
   PARSENAME(REPLACE(OwnerAddress, ',', '.') , 1)
 FROM 
-  Nashville_Housing_Data_for_Data_Cleaning  
+  Nashville_Housing_Data  
 
 ALTER TABLE 
-  Nashville_Housing_Data_for_Data_Cleaning 
+  Nashville_Housing_Data
 ADD COLUMN
   OwnerStreetAddress Nvarchar(255);
 
 Update 
-  Nashville_Housing_Data_for_Data_Cleaning 
+  Nashville_Housing_Data
 SET OwnerStreetAddress = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 3)
 
 
 ALTER TABLE 
-  Nashville_Housing_Data_for_Data_Cleaning 
+  Nashville_Housing_Datag 
 Add OwnerCity Nvarchar(255);
 
 Update 
-  Nashville_Housing_Data_for_Data_Cleaning 
+  Nashville_Housing_Data 
 SET OwnerCity = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 2)
 
 
 ALTER TABLE 
-  Nashville_Housing_Data_for_Data_Cleaning 
+  Nashville_Housing_Data
 ADD COLUMN 
   OwnerState Nvarchar(255);
 
 UPDATE
-  Nashville_Housing_Data_for_Data_Cleaning 
+  Nashville_Housing_Data
 SET OwnerState = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 1)
 
 -- SQLite Method: Splitting into 3 columns
 
 SEELCT *
-FROM Nashville_Housing_Data_for_Data_Cleaning 
+FROM Nashville_Housing_Data 
   
---Use CTE to find Address Comma positions & Split into 3 columns 
+--Use CTE to find Address Comma positions & Split the address into 3 columns 
 WITH AddressParts AS (
   SELECT
     OwnerAddress,
@@ -280,7 +281,7 @@ WITH AddressParts AS (
     	 * cont: Equates to instr(subtring extracted, ',') for the 2nd comma position value
     	 * 2nd half: adds on the 1st comman position value, getting 2nd comma's ACTUAL POSITION in the whole address length
     	 */
-  FROM Nashville_Housing_Data_for_Data_Cleaning
+  FROM Nashville_Housing_Data
 )
 SELECT
   SUBSTR(OwnerAddress, 1, Comma1Pos - 1) AS OwnerStreetAddress, -- extract starts from the 1st character, ends at -1 to omit the ',' 
@@ -290,33 +291,33 @@ SELECT
 FROM AddressParts;
 
 ALTER TABLE
-  Nashville_Housing_Data_for_Data_Cleaning
+  Nashville_Housing_Data
 ADD COLUMN
   OwnerStreetAddress TEXT;
   
 ALTER TABLE
-  Nashville_Housing_Data_for_Data_Cleaning
+  Nashville_Housing_Data
 ADD COLUMN
   OwnerCity TEXT;
 
 ALTER TABLE
-  Nashville_Housing_Data_for_Data_Cleaning
+  Nashville_Housing_Data
 ADD COLUMN
   OwnerState TEXT;
 
 -- Update StreetAddress, City, and State columns
 UPDATE 
-  Nashville_Housing_Data_for_Data_Cleaning
+  Nashville_Housing_Data
 SET 
   OwnerStreetAddress = SUBSTR(OwnerAddress, 1, instr(OwnerAddress, ',') - 1)
 
 UPDATE 
-  Nashville_Housing_Data_for_Data_Cleaning
+  Nashville_Housing_Data
 SET 
   OwnerCity = SUBSTR(OwnerAddress, INSTR(OwnerAddress, ',')+2, (INSTR(SUBSTR(OwnerAddress, INSTR(OwnerAddress, ',') + 1), ',') - 1))  
   
 UPDATE 
-  Nashville_Housing_Data_for_Data_Cleaning
+  Nashville_Housing_Data
 SET 
   OwnerState = SUBSTR(OwnerAddress, (INSTR(SUBSTR(OwnerAddress, INSTR(OwnerAddress, ',') + 1), ',') + INSTR(OwnerAddress, ',')+2))
 
@@ -328,7 +329,7 @@ SELECT
   DISTINCT(SoldAsVacant),
   COUNT(SoldAsVacant)
 FROM
-  Nashville_Housing_Data_for_Data_Cleaning  
+  Nashville_Housing_Data 
 GROUP BY
   SoldAsVacant
 ORDER BY 
@@ -342,10 +343,10 @@ SELECT
   	ELSE SoldAsVacant
   END
 FROM
-  Nashville_Housing_Data_for_Data_Cleaning
+  Nashville_Housing_Data
   
 UPDATE 
-  Nashville_Housing_Data_for_Data_Cleaning 
+  Nashville_Housing_Data 
 SET 
   SoldAsVacant = CASE 
   	WHEN SoldAsVacant = 'Y'THEN 'Yes'
@@ -370,7 +371,7 @@ SELECT
   				ORDER BY
   					"UniqueID ") AS row_num 
 FROM
-  Nashville_Housing_Data_for_Data_Cleaning 
+  Nashville_Housing_Data 
 )
 SELECT
   *
@@ -395,10 +396,10 @@ SELECT
   				ORDER BY
   					"UniqueID ") AS row_num 
 FROM
-  Nashville_Housing_Data_for_Data_Cleaning 
+  Nashville_Housing_Data
 )
 DELETE FROM 
-  Nashville_Housing_Data_for_Data_Cleaning
+  Nashville_Housing_Data
 WHERE 
   ROWID IN (
   SELECT
@@ -413,7 +414,7 @@ WHERE
  * always ask seniors before proceeding) */
   
 ALTER TABLE
-  Nashville_Housing_Data_for_Data_Cleaning 
+  Nashville_Housing_Data
 DROP COLUMN
   SaleDate,
   PropertyAddress
